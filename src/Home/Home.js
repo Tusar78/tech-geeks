6f8cf19eec7063edde6ef065a8a47840449b7754
Tusar <tusarahmed141@gmail.com>
2022-08-21 14:11:30 +0600

    Make a home component

---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className='bg-purple-400 text-white font-bold font-mono'>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
