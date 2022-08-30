075f00c9e352e76b34b1964cb5798d6d769238d1
Tusar <tusarahmed141@gmail.com>
2022-08-30 23:55:46 +0600

    Added password group

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -8,8 +8,12 @@ const Login = () => {
           <h2 className="form__title">Login</h2>
 
           <div className="form__group">
-            <label htmlFor="email" className='form__email'>Email</label>
+            <label htmlFor="email" className='form__label'>Email</label>
             <input type="email" name="email" id="email" className='form__input email' />
+          </div>
+          <div className="form__group">
+            <label htmlFor="password" className='form__label'>Email</label>
+            <input type="password" name="password" id="password" className='form__input password' />
           </div>
         </form>
       </div>
