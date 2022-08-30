f51fcb8adc2d5a619377d54d1eeeb3db9401784c
Tusar <tusarahmed141@gmail.com>
2022-08-31 01:15:30 +0600

    Login section done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,38 +1,40 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
     <section className="section">
       <div className="form-container custom-grid">
-        <form className="form">
-          <h2 className="form__title">Login</h2>
+        <div className="form">
+          <form>
+            <h2 className="form__title">Login</h2>
+            <div className="form__group">
+              <label htmlFor="email" className="form__label">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="form__input email"
+              />
+            </div>
+            <div className="form__group">
+              <label htmlFor="password" className="form__label">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="form__input password"
+              />
+            </div>
 
-          <div className="form__group">
-            <label htmlFor="email" className="form__label">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="form__input email"
-            />
-          </div>
-          <div className="form__group">
-            <label htmlFor="password" className="form__label">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="form__input password"
-            />
-          </div>
-
-          <button type="submit" className="form__button">
-            Login
-          </button>
+            <button type="submit" className="form__button">
+              Login
+            </button>
+          </form>
 
           <div className="form__login-register">
             <span className="form__loginRegister-demo">New To Tech Geeks?</span>
@@ -51,9 +53,13 @@ const Login = () => {
               </label>
             </div>
           </div>
-
           <p className="form__divider">or</p>
-        </form>
+
+          <button className="form__google-btn">
+            <FcGoogle className="form__google-btn-icon" />
+            <span>Continue with Google</span>
+          </button>
+        </div>
       </div>
     </section>
   );
