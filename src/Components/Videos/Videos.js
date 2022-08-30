a96c310e8f19ec0a9526735bb09e63309a5e9dec
Tusar <tusarahmed141@gmail.com>
2022-08-30 16:52:27 +0600

    Video section markup done

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Videos = () => {
   return (
-    <section className='section'>
-      
+    <section className="section">
+      <div className="video__container custom-grid">
+        <div className="video__head">
+          <h2 className="video__title">Introducing iPhone 13 Pro</h2>
+          <h5 className="video__subtitle">
+            iPhone 13 just got release. Here is what you need to know about the
+            new iPhone.
+          </h5>
+        </div>
+        <div className="video__body">
+          <iframe
+            className="video__player"
+            src="https://www.youtube.com/embed/XKfgdkcIUxw"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
     </section>
   );
 };
