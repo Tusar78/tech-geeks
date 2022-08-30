bb167090a86739797a073f96ef5016a577a961b6
Tusar <tusarahmed141@gmail.com>
2022-08-30 23:54:42 +0600

    Added email group

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h2>Hello Login</h2>
-    </div>
+    <section className='section'>
+      <div className="form-container">
+        <form className='form'>
+          <h2 className="form__title">Login</h2>
+
+          <div className="form__group">
+            <label htmlFor="email" className='form__email'>Email</label>
+            <input type="email" name="email" id="email" className='form__input email' />
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
