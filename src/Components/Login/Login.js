f76f9a03fbb3f10a60fe4df2709e42eb84bb1af2
Tusar <tusarahmed141@gmail.com>
2022-09-18 18:26:11 +0600

    Collect img and password value

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
 import { auth } from "../../firebase.init.js";
@@ -7,6 +8,8 @@ const Login = () => {
   const [toggle, setToggle] = useState(true);
   const googleProvider = new GoogleAuthProvider();
 
+  const navigate = useNavigate();
+
   const handleCheckbox = (e) => {
     setToggle(e.target.checked);
   };
@@ -14,8 +17,9 @@ const Login = () => {
   const handleGoogleProvider = () => {
     signInWithPopup(auth, googleProvider)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        // const user = result.user;
+        // console.log(user);
+        navigate("/home");
       })
       .catch((error) => {
         const errorMessage = error.message;
@@ -25,85 +29,85 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
   };
 
   return (
     <section className="section">
       <div className="form-container custom-grid">
         <div className="form">
-          <form>
-            <h2 className="form__title">{toggle ? "Login" : "Sign Up"}</h2>
-            {toggle ? (
-              <>
-                <div className="form__group">
-                  <label htmlFor="email" className="form__label">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="form__input email"
-                  />
-                </div>
-                <div className="form__group">
-                  <label htmlFor="password" className="form__label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    className="form__input password"
-                  />
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="form__group">
-                  <label htmlFor="email" className="form__label">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="form__input email"
-                  />
-                </div>
-                <div className="form__group">
-                  <label htmlFor="password" className="form__label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    className="form__input password"
-                  />
-                </div>
-                <div className="form__group">
-                  <label htmlFor="confirmedPassword" className="form__label">
-                    Confirmed Password
-                  </label>
-                  <input
-                    type="password"
-                    name="confirmed-password"
-                    id="confirmedPassword"
-                    className="form__input confirmed-password"
-                  />
-                </div>
-              </>
-            )}
-
-            <button
-              type="submit"
-              className="form__button"
-              onClick={handleSubmit}
-            >
-              {toggle ? "Login" : "Sign Up"}
-            </button>
-          </form>
+          {toggle ? (
+            <form>
+              <h2 className="form__title">Login</h2>
+              <div className="form__group">
+                <label htmlFor="email" className="form__label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form__input email"
+                />
+              </div>
+              <div className="form__group">
+                <label htmlFor="password" className="form__label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="form__input password"
+                />
+              </div>
+              <button type="submit" className="form__button">
+                Login
+              </button>
+            </form>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <h2 className="form__title">Sign Up</h2>
+              <div className="form__group">
+                <label htmlFor="email" className="form__label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form__input email"
+                />
+              </div>
+              <div className="form__group">
+                <label htmlFor="password" className="form__label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="form__input password"
+                />
+              </div>
+              <div className="form__group">
+                <label htmlFor="confirmedPassword" className="form__label">
+                  Confirmed Password
+                </label>
+                <input
+                  type="password"
+                  name="confirmed-password"
+                  id="confirmedPassword"
+                  className="form__input confirmed-password"
+                />
+              </div>
+              <button type="submit" className="form__button">
+                Sign Up
+              </button>
+            </form>
+          )}
 
           <div className="form__login-register">
             <span className="form__loginRegister-demo">
